0c024da9ed3b6fd73425694faf273c2382f84858
Augusto <aleal4@gmail.com>
2022-07-23 15:34:10 -0700

    map must be in ()

--- src/Components/common/pagination.jsx
+++ src/Components/common/pagination.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import _ from 'lodash';
 
 const Pagination = (props) => {
-  return null;
+  // destructure props object
+  const { pageSize, itemsCount } = props;
+  // find out how many pages for all content to load depending on page size limits and itemCount
+  const pagesCount = itemsCount / pageSize;
+  // use lodash to create page range
+  const pages = _.range(1, pagesCount + 1);
+  return (
+    <nav>
+      <ul className="pagination">
+        {pages.map((page) => (
+          <li key={page} className="page-item">
+            <a href="#" className="page-link">
+              {page}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 };
 
 export default Pagination;
