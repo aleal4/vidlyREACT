ecc455fe18b153ec48a098695db6c6b2fd0cb84e
Augusto <aleal4@gmail.com>
2022-07-22 18:32:25 -0700

    slowly learning

--- src/Components/Movies.jsx
+++ src/Components/Movies.jsx
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
-import { getMovies } from '../services/fakeMovieService';
-import Header from '../Components/Header';
-import Movie from '../Components/Movie';
+import { getMovies, deleteMovies } from '../services/fakeMovieService';
+// import Header from '../Components/Header';
+// import Movie from '../Components/Movie';
 
 class Movies extends Component {
   state = {
     movies: getMovies(),
   };
-  handleDelete = (movie) => {};
+
+  handleDelete = (movie) => {
+    const movies = this.state.movies.filter((ele) => ele._id !== movie._id);
+    this.setState({ movies: movies });
+  };
 
   render() {
     return (
@@ -18,15 +22,24 @@ class Movies extends Component {
             <th>Genre</th>
             <th>Stock</th>
             <th>Rate</th>
+            <th></th>
           </tr>
         </thead>
         <tbody>
           {this.state.movies.map((movie) => (
-            <tr>
+            <tr key={movie._id}>
               <td>{movie.title}</td>
               <td>{movie.genre.name}</td>
               <td>{movie.numberInStock}</td>
               <td>{movie.dailyRentalRate}</td>
+              <td>
+                <button
+                  onClick={() => this.handleDelete(movie)}
+                  className="btn btn-danger btn-sm"
+                >
+                  Delete
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
