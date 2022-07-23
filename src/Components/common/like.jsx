6a286b6cdb0fd7e6d0b04cf65bec233787f2cc2e
Augusto <aleal4@gmail.com>
2022-07-22 22:43:33 -0700

    heart icon

---
+++ src/Components/common/like.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+// Input: liked: boolean
+// Output: onClick
+
+class Like extends Component {
+  render() {
+    return <i class="fa fa-heart" aria-hidden="true"></i>;
+  }
+}
+
+export default Like;
