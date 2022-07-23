02cfba539eb42f4899f180f0e597b9dd416cf393
Augusto <aleal4@gmail.com>
2022-07-23 15:46:47 -0700

    pagination continued

--- src/Components/Movies.jsx
+++ src/Components/Movies.jsx
@@ -9,6 +9,7 @@ class Movies extends Component {
   state = {
     movies: getMovies(),
     pageSize: 4,
+    currentPage: 1,
   };
 
   handleDelete = (movie) => {
@@ -25,11 +26,12 @@ class Movies extends Component {
   };
 
   handlePageChange = (page) => {
-    console.log(page);
+    this.setState({ currentPage: page });
   };
 
   render() {
     const { length: count } = this.state.movies;
+    const { pageSize, currentPage } = this.state;
 
     if (count === 0) return <p>No movies in the database</p>;
 
@@ -74,7 +76,8 @@ class Movies extends Component {
         </table>
         <Pagination
           itemsCount={count}
-          pageSize={this.state.pageSize}
+          pageSize={pageSize}
+          currentPage={currentPage}
           onPageChange={this.handlePageChange}
         />
       </React.Fragment>
--- src/Components/common/pagination.jsx
+++ src/Components/common/pagination.jsx
@@ -3,18 +3,22 @@ import _ from 'lodash';
 
 const Pagination = (props) => {
   // destructure props object
-  const { pageSize, itemsCount } = props;
+  const { pageSize, itemsCount, currentPage, onPageChange } = props;
+  console.log(currentPage);
   // find out how many pages for all content to load depending on page size limits and itemCount
   const pagesCount = Math.ceil(itemsCount / pageSize);
-  // use lodash to create page range
   if (pagesCount === 1) return null;
+  // use lodash to create page range
   const pages = _.range(1, pagesCount + 1);
   return (
     <nav>
       <ul className="pagination">
         {pages.map((page) => (
-          <li key={page} className="page-item">
-            <a href="#" className="page-link">
+          <li
+            key={page}
+            className={page === currentPage ? 'page-item active' : 'page-item'}
+          >
+            <a onClick={() => onPageChange(page)} className="page-link">
               {page}
             </a>
           </li>
