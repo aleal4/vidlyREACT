861a43e699f2d19b5a48e1eb91bd24c13dcd4dad
Augusto <aleal4@gmail.com>
2022-07-22 23:03:58 -0700

    heart button liked

--- src/Components/common/like.jsx
+++ src/Components/common/like.jsx
@@ -3,19 +3,17 @@ import React, { Component } from 'react';
 // Input: liked: boolean
 // Output: onClick
 
-class Like extends Component {
-  render() {
-    let classes = 'fa fa-heart';
-    if (!this.props.liked) classes += '-o';
-    return (
-      <i
-        onClick={this.props.onClick}
-        className={classes}
-        aria-hidden="true"
-        style={{ cursor: 'pointer' }}
-      ></i>
-    );
-  }
-}
+const Like = (props) => {
+  let classes = 'fa fa-heart';
+  if (!props.liked) classes += '-o';
+  return (
+    <i
+      onClick={props.onClick}
+      className={classes}
+      aria-hidden="true"
+      style={{ cursor: 'pointer' }}
+    ></i>
+  );
+};
 
 export default Like;
